57f8787538bfd4bd1e38312c4775e26ebe816cf9
Francois Suter <francois@typo3.org>
2015-06-16 16:51:53 +0200

    [FEATURE] New debug output styles
    A new styling is introduced for the debug output, which is
    independent of TYPO3 Core skins, to avoid breaking upon
    each major release.
    
    Font Awesome is added for having scalable icons.
    
    Resolves: #67467
    Releases: 2.0
    Change-Id: Ibff9221b2942e4a66b8dcab33cfc0a293e5649d8
    Reviewed-on: http://review.typo3.org/40309
    Reviewed-by: Francois Suter <francois@typo3.org>
    Tested-by: Francois Suter <francois@typo3.org>

--- Classes/Utility/Debugger.php
+++ Classes/Utility/Debugger.php
@@ -54,22 +54,40 @@ class Debugger implements SingletonInterface {
 
 	public function __construct(PageRenderer $pageRenderer) {
 		$this->pageRenderer = $pageRenderer;
-		// Prepare CSS code based on t3skin, if loaded
-		if (ExtensionManagementUtility::isLoaded('t3skin')) {
-			$this->cssCode = GeneralUtility::getUrl(ExtensionManagementUtility::extPath('displaycontroller') . 'Resources/Public/Styles/Debugger.css');
-			$t3SkinPath = ExtensionManagementUtility::extPath('t3skin');
-			$messageSkinningFile = $t3SkinPath  . 'Resources/Public/Css/visual/element_message.css';
-			$pathToReplace = '../../../../icons';
-			$this->cssCode .= GeneralUtility::getUrl($messageSkinningFile);
-			// Adjust path to icons
-			$replacement = GeneralUtility::locationHeaderUrl(TYPO3_mainDir . ExtensionManagementUtility::extRelPath('t3skin') . 'icons');
-			$this->cssCode = str_replace($pathToReplace, $replacement, $this->cssCode);
-			$this->jsFile = GeneralUtility::locationHeaderUrl(TYPO3_mainDir . ExtensionManagementUtility::extRelPath('displaycontroller')) . 'Resources/Public/JavaScript/Debugger.js';
-		}
+		$extensionRelativePath = ExtensionManagementUtility::extRelPath('displaycontroller');
+		// Load the relevant CSS and JS files (making sure they are not concatenated)
+		$pageRenderer->addCssFile(
+			$extensionRelativePath . 'Resources/Public/Styles/font-awesome/css/font-awesome.min.css',
+			'stylesheet',
+			'screen',
+			'',
+			FALSE,
+			FALSE,
+			'',
+			TRUE
+		);
+		$pageRenderer->addCssFile(
+			$extensionRelativePath . 'Resources/Public/Styles/Debugger.css',
+			'stylesheet',
+			'screen',
+			'',
+			FALSE,
+			FALSE,
+			'',
+			TRUE
+		);
+		$pageRenderer->addJsFile(
+			$extensionRelativePath . 'Resources/Public/JavaScript/Debugger.js',
+			'text/javascript',
+			FALSE,
+			FALSE,
+			'',
+			TRUE
+		);
 	}
 
 	/**
-	 * Renders all messages and dumps their related data
+	 * Renders all messages and dumps their related data.
 	 *
 	 * @param array $messageQueue List of messages to display
 	 * @return string Debug output
@@ -77,38 +95,40 @@ class Debugger implements SingletonInterface {
 	public function render(array $messageQueue) {
 		$debugOutput = '';
 		if (count($messageQueue) > 0) {
-			// If this is the first debug call, write the necessary CSS code
-			// and load the related JS library
-			if ($this->firstCall) {
-				$debugOutput .= '<style>' . $this->cssCode . '</style>';
-				$debugOutput .= '<script src="' . $this->jsFile . '" type="text/javascript"></script>';
-				$this->firstCall = FALSE;
-			}
 			// Prepare the output and return it
 			$icons = '';
 			foreach ($messageQueue as $messageData) {
 				/** @var \TYPO3\CMS\Core\Messaging\FlashMessage $messageObject */
 				$messageObject = $messageData['message'];
-				// Choose the log method based on severity
+				// Choose the display classes and the log method based on severity
+				$logMethod = 'log';
 				switch ($messageObject->getSeverity()) {
 					case 2:
 						$logMethod = 'error';
+						$classes = 'debug-message debug-error fa fa-times-circle';
 						break;
 					case 1:
 						$logMethod = 'warn';
+						$classes = 'debug-message debug-warning fa fa-exclamation-triangle';
+						break;
+					case 0:
+						$classes = 'debug-message debug-success fa fa-check-circle';
+						break;
+					case -1:
+						$classes = 'debug-message debug-info fa fa-info-circle';
 						break;
 					default:
-						$logMethod = 'log';
+						$classes = 'debug-message debug-note fa fa-comment';
 				}
 
 				// Prepare the output, as a clickable icon and a message
 				$label = '<p><strong>' . $messageObject->getTitle() . '</strong>: ' . $messageObject->getMessage() . '</p>';
 				$debugLink = '
-					<span class="debug-message ' . $messageObject->getClass() . '"' .
+					<i class="' . $classes . '"' .
 					' data-debug="' . urlencode(json_encode($messageData['data'])) . '"' .
 					' data-debug-header="' . addslashes($messageObject->getTitle() . ': ' . $messageObject->getMessage()) . '"' .
 					' data-method="' . $logMethod . '"' .
-					' onclick="DisplaycontrollerDebugger.dumpDebugData()">&nbsp;</span>
+					' onclick="DisplaycontrollerDebugger.dumpDebugData()"></i>
 				';
 				$icons .= '<div class="icon-group">' . $debugLink . $label . '</div>';
 			}
